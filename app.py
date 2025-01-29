650453e7fbfc3ad84bcc107dd04a0c250f932157
Kyle Root <kyle@wellconnected.co>
2025-01-29 11:46:57 -0500

    adding comment

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from streamlit_cookies_manager import EncryptedCookieManager
 
+# TODO: **Update streamlit_cookies_manager**: The behavior of `st.cache` was updated in Streamlit 1.36 to the new caching
+# logic used by `st.cache_data` and `st.cache_resource`. This might lead to some problems
+# or unexpected behavior in certain edge cases.
+
 # Initialize the cookie manager
 st.session_state.cookies = EncryptedCookieManager(prefix="thrive_ai_", password=st.secrets["cookie"]["password"])
 
