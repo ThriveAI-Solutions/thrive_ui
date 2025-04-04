a0d0c7074b78018878610bfebf6e3de5e992c1f4
Rob Enderle <robenderleamd@gmail.com>
2025-04-04 11:20:11 -0400

    added documentation to training step

--- utils/vanna_calls.py
+++ utils/vanna_calls.py
@@ -1,52 +1,80 @@
-import streamlit as st
-from vanna.remote import VannaDefault
 import json
-import psycopg2
-from psycopg2.extras import RealDictCursor
 from pathlib import Path
+
+import psycopg2
 import sqlparse
-from sqlparse.sql import IdentifierList, Identifier
-from sqlparse.tokens import Keyword, DML
-from vanna.vannadb import VannaDB_VectorStore
+import streamlit as st
+from psycopg2.extras import RealDictCursor
+from sqlparse.sql import Identifier, IdentifierList
+from sqlparse.tokens import DML, Keyword
 from vanna.anthropic import Anthropic_Chat
-from vanna.ollama import Ollama
 from vanna.chromadb import ChromaDB_VectorStore
+from vanna.ollama import Ollama
+from vanna.remote import VannaDefault
+from vanna.vannadb import VannaDB_VectorStore
+
 
 class MyVannaAnthropic(VannaDB_VectorStore, Anthropic_Chat):
     def __init__(self, config=None):
         try:
-            print('Using Anthropic and VannaDB')
-            VannaDB_VectorStore.__init__(self, vanna_model=st.secrets["ai_keys"]["vanna_model"], vanna_api_key=st.secrets["ai_keys"]["vanna_api"], config=config)
-            Anthropic_Chat.__init__(self, config={'api_key': st.secrets["ai_keys"]["anthropic_api"], 'model': st.secrets["ai_keys"]["anthropic_model"]})
+            print("Using Anthropic and VannaDB")
+            VannaDB_VectorStore.__init__(
+                self,
+                vanna_model=st.secrets["ai_keys"]["vanna_model"],
+                vanna_api_key=st.secrets["ai_keys"]["vanna_api"],
+                config=config,
+            )
+            Anthropic_Chat.__init__(
+                self,
+                config={
+                    "api_key": st.secrets["ai_keys"]["anthropic_api"],
+                    "model": st.secrets["ai_keys"]["anthropic_model"],
+                },
+            )
         except Exception as e:
             print(f"Error Configuring MyVannaAnthropic: {e}")
+
 
 class MyVannaAnthropicChromaDB(ChromaDB_VectorStore, Anthropic_Chat):
     def __init__(self, config=None):
         try:
-            print('Using Anthropic and chromaDB')
-            ChromaDB_VectorStore.__init__(self, config={'path': st.secrets["rag_model"]["chroma_path"]})
-            Anthropic_Chat.__init__(self, config={'api_key': st.secrets["ai_keys"]["anthropic_api"], 'model': st.secrets["ai_keys"]["anthropic_model"]})
+            print("Using Anthropic and chromaDB")
+            ChromaDB_VectorStore.__init__(self, config={"path": st.secrets["rag_model"]["chroma_path"]})
+            Anthropic_Chat.__init__(
+                self,
+                config={
+                    "api_key": st.secrets["ai_keys"]["anthropic_api"],
+                    "model": st.secrets["ai_keys"]["anthropic_model"],
+                },
+            )
         except Exception as e:
             print(f"Error Configuring MyVannaAnthropicChromaDB: {e}")
+
 
 class MyVannaOllama(VannaDB_VectorStore, Ollama):
     def __init__(self, config=None):
         try:
-            print('Using Ollama and VannaDB')
-            VannaDB_VectorStore.__init__(self, vanna_model=st.secrets["ai_keys"]["vanna_model"], vanna_api_key=st.secrets["ai_keys"]["vanna_api"], config=config)
-            Ollama.__init__(self, config={'model': st.secrets["ai_keys"]["ollama_model"]})
+            print("Using Ollama and VannaDB")
+            VannaDB_VectorStore.__init__(
+                self,
+                vanna_model=st.secrets["ai_keys"]["vanna_model"],
+                vanna_api_key=st.secrets["ai_keys"]["vanna_api"],
+                config=config,
+            )
+            Ollama.__init__(self, config={"model": st.secrets["ai_keys"]["ollama_model"]})
         except Exception as e:
             print(f"Error Configuring MyVannaOllama: {e}")
+
 
 class MyVannaOllamaChromaDB(ChromaDB_VectorStore, Ollama):
     def __init__(self, config=None):
         try:
-            print('Using Ollama and ChromaDB')
-            ChromaDB_VectorStore.__init__(self, config={'path': st.secrets["rag_model"]["chroma_path"]})
-            Ollama.__init__(self, config={'model': st.secrets["ai_keys"]["ollama_model"]})
+            print("Using Ollama and ChromaDB")
+            ChromaDB_VectorStore.__init__(self, config={"path": st.secrets["rag_model"]["chroma_path"]})
+            Ollama.__init__(self, config={"model": st.secrets["ai_keys"]["ollama_model"]})
         except Exception as e:
             print(f"Error Configuring MyVannaOllamaChromaDB: {e}")
+
 
 def read_forbidden_from_json():
     try:
@@ -64,8 +92,10 @@ def read_forbidden_from_json():
         print(f"Error reading forbidden_references.json: {e}")
         return [], []
 
+
 forbidden_tables, forbidden_columns = read_forbidden_from_json()
 forbidden_tables_str = ", ".join(f"'{table}'" for table in forbidden_tables)
+
 
 @st.cache_resource(ttl=3600)
 def setup_vanna():
@@ -85,20 +115,21 @@ def setup_vanna():
             else:
                 raise ValueError("Missing anthropic Configuration Values")
         else:
-            print('Using Default')
+            print("Using Default")
             vn = VannaDefault(api_key=st.secrets["ai_keys"]["vanna_api"], model=st.secrets["ai_keys"]["vanna_model"])
 
         vn.connect_to_postgres(
             host=st.secrets["postgres"]["host"],
             dbname=st.secrets["postgres"]["database"],
             user=st.secrets["postgres"]["user"],
-            password=st.secrets["postgres"]["password"], 
-            port=st.secrets["postgres"]["port"]
+            password=st.secrets["postgres"]["password"],
+            port=st.secrets["postgres"]["port"],
         )
         return vn
     except Exception as e:
         st.error(f"Error setting up Vanna: {e}")
         print(e)
+
 
 @st.cache_data(show_spinner="Generating sample questions ...")
 def generate_questions_cached():
@@ -109,12 +140,16 @@ def generate_questions_cached():
         st.error(f"Error generating questions: {e}")
         print(e)
 
+
 @st.cache_data(show_spinner="Generating SQL query ...")
 def generate_sql_cached(question: str):
     try:
         vn = setup_vanna()
 
-        if "allow_llm_to_see_data" in st.secrets.security and bool(st.secrets.security["allow_llm_to_see_data"]) == True:
+        if (
+            "allow_llm_to_see_data" in st.secrets.security
+            and bool(st.secrets.security["allow_llm_to_see_data"]) == True
+        ):
             print("Allowing LLM to see data")
             return check_references(vn.generate_sql(question=question, allow_llm_to_see_data=True))
         else:
@@ -123,6 +158,7 @@ def generate_sql_cached(question: str):
     except Exception as e:
         st.error(f"Error generating SQL: {e}")
         print(e)
+
 
 @st.cache_data(show_spinner="Checking for valid SQL ...")
 def is_sql_valid_cached(sql: str):
@@ -133,6 +169,7 @@ def is_sql_valid_cached(sql: str):
         st.error(f"Error checking SQL validity: {e}")
         print(e)
 
+
 @st.cache_data(show_spinner="Running SQL query ...")
 def run_sql_cached(sql: str):
     try:
@@ -142,6 +179,7 @@ def run_sql_cached(sql: str):
         st.error(f"Error running SQL: {e}")
         print(e)
 
+
 @st.cache_data(show_spinner="Checking if we should generate a chart ...")
 def should_generate_chart_cached(question, sql, df):
     try:
@@ -150,6 +188,7 @@ def should_generate_chart_cached(question, sql, df):
     except Exception as e:
         st.error(f"Error checking if we should generate a chart: {e}")
         print(e)
+
 
 @st.cache_data(show_spinner="Generating Plotly code ...")
 def generate_plotly_code_cached(question, sql, df):
@@ -161,6 +200,7 @@ def generate_plotly_code_cached(question, sql, df):
         st.error(f"Error generating Plotly code: {e}")
         print(e)
 
+
 @st.cache_data(show_spinner="Running Plotly code ...")
 def generate_plot_cached(code, df):
     try:
@@ -169,6 +209,7 @@ def generate_plot_cached(code, df):
     except Exception as e:
         st.error(f"Error generating Plotly chart: {e}")
         print(e)
+
 
 @st.cache_data(show_spinner="Generating followup questions ...")
 def generate_followup_cached(question, sql, df):
@@ -179,6 +220,7 @@ def generate_followup_cached(question, sql, df):
         st.error(f"Error generating followup questions: {e}")
         print(e)
 
+
 @st.cache_data(show_spinner="Generating summary ...")
 def generate_summary_cached(question, df):
     try:
@@ -187,6 +229,7 @@ def generate_summary_cached(question, df):
     except Exception as e:
         st.error(f"Error generating summary: {e}")
         print(e)
+
 
 def remove_from_file_training(new_entry: dict):
     try:
@@ -207,17 +250,20 @@ def remove_from_file_training(new_entry: dict):
         existing_questions = {entry["question"] for entry in training_data["sample_queries"]}
         if new_entry["question"] in existing_questions:
             # Remove the new entry from the sample_queries list if it's not a duplicate
-            training_data["sample_queries"] = [entry for entry in training_data["sample_queries"] if entry["question"] != new_entry["question"]]
+            training_data["sample_queries"] = [
+                entry for entry in training_data["sample_queries"] if entry["question"] != new_entry["question"]
+            ]
 
             # Write the updated data back to the file
             with training_file_path.open("w") as file:
                 json.dump(training_data, file, indent=4)
 
-            print('Entry removed from training_data.json')
+            print("Entry removed from training_data.json")
         else:
-            print('Entry not found, nothing removed from training_data.json')
+            print("Entry not found, nothing removed from training_data.json")
     except Exception as e:
         st.error(f"Error removing entry from training_data.json: {e}")
+
 
 def write_to_file_and_training(new_entry: dict):
     try:
@@ -242,12 +288,13 @@ def write_to_file_and_training(new_entry: dict):
             with training_file_path.open("w") as file:
                 json.dump(training_data, file, indent=4)
 
-            print('New entry added to training_data.json')
+            print("New entry added to training_data.json")
         else:
-            print('Duplicate entry found. No new entry added.')
+            print("Duplicate entry found. No new entry added.")
     except Exception as e:
         st.error(f"Error writing to training_data.json: {e}")
         print(e)
+
 
 # Train Vanna on database schema
 @st.cache_resource
@@ -262,7 +309,7 @@ def train_ddl():
             database=st.secrets["postgres"]["database"],
             user=st.secrets["postgres"]["user"],
             password=st.secrets["postgres"]["password"],
-            cursor_factory=RealDictCursor
+            cursor_factory=RealDictCursor,
         )
 
         # Get database schema
@@ -284,48 +331,49 @@ def train_ddl():
                 table_schema, table_name, ordinal_position;
         """)
         schema_info = cursor.fetchall()
-        
+
         # Format schema for training
         ddl = []
         current_table = None
         for row in schema_info:
-            if current_table != row['table_name']:
+            if current_table != row["table_name"]:
                 if current_table is not None:
-                    ddl.append(');')
+                    ddl.append(");")
                     # Train vanna with schema and queries
-                    vn.train(ddl=' '.join(ddl))
-                    ddl = [] # reset ddl for next table
-                current_table = row['table_name']
+                    vn.train(ddl=" ".join(ddl))
+                    ddl = []  # reset ddl for next table
+                current_table = row["table_name"]
                 ddl.append(f"\nCREATE TABLE {row['table_name']} (")
             else:
-                ddl.append(',')
-            
-            nullable = "NULL" if row['is_nullable'] == 'YES' else "NOT NULL"
+                ddl.append(",")
+
+            nullable = "NULL" if row["is_nullable"] == "YES" else "NOT NULL"
             ddl.append(f"\n    {row['column_name']} {row['data_type']} {nullable}")
-        
+
         if ddl:  # Close the last table
-            ddl.append(');')
+            ddl.append(");")
             # Train vanna with schema and queries
-            vn.train(ddl=' '.join(ddl))
-            ddl = [] # reset ddl for next table
-        
+            vn.train(ddl=" ".join(ddl))
+            ddl = []  # reset ddl for next table
+
         cursor.close()
         conn.close()
     except Exception as e:
         st.error(f"Error training DDL: {e}")
         print(e)
 
+
 # Train Vanna on database question/query pairs from file
 @st.cache_resource
 def train_file():
     try:
         vn = setup_vanna()
-        
+
         # Load training queries from JSON
-        training_file = Path(__file__).parent / 'config' / 'training_data.json'
-        with open(training_file, 'r') as f:
+        training_file = Path(__file__).parent / "config" / "training_data.json"
+        with open(training_file, "r") as f:
             training_data = json.load(f)
-        
+
         # Extract the sample queries
         sample_queries = training_data.get("sample_queries", [])
 
@@ -335,9 +383,19 @@ def train_file():
             query = query.get("query")
             if question and query:
                 vn.train(question=question, sql=query)
+
+        # Extract the sample documents
+        sample_documents = training_data.get("sample_documents", [])
+
+        # Iterate over the sample documents and send the documentation to vn.train()
+        for doc in sample_documents:
+            documentation = doc.get("documentation")
+            if documentation:
+                vn.train(documentation=documentation)
     except Exception as e:
         st.error(f"Error training from file: {e}")
         print(e)
+
 
 def is_table_token(token):
     try:
@@ -352,15 +410,16 @@ def is_table_token(token):
         st.error(f"Error training from file: {e}")
         print(e)
 
+
 def get_identifiers(parsed):
     try:
         tables = []
         columns = []
         is_table_context = False
         for token in parsed.tokens:
-            if token.ttype in Keyword and token.value.upper() in ('FROM', 'JOIN', 'INTO', 'UPDATE'):
+            if token.ttype in Keyword and token.value.upper() in ("FROM", "JOIN", "INTO", "UPDATE"):
                 is_table_context = True
-            elif token.ttype in Keyword and token.value.upper() in ('SELECT', 'WHERE', 'GROUP BY', 'ORDER BY'):
+            elif token.ttype in Keyword and token.value.upper() in ("SELECT", "WHERE", "GROUP BY", "ORDER BY"):
                 is_table_context = False
 
             if isinstance(token, IdentifierList):
@@ -379,12 +438,13 @@ def get_identifiers(parsed):
         st.error(f"Error getting identifiers: {e}")
         print(e)
 
+
 def check_references(sql):
     try:
         # TODO: should I make this role based? or user based?
         parsed = sqlparse.parse(sql)[0]
         tables, columns = get_identifiers(parsed)
-        
+
         # Check for forbidden references
         referenced_tables = set(forbidden_tables).intersection(set(tables))
         referenced_columns = set(forbidden_columns).intersection(set(columns))
