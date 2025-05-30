7a5229b4120ef073e0f240d132fd20e441e1fe76
Rob Enderle <robenderleamd@gmail.com>
2025-05-30 16:55:44 -0400

    switched from pg to sqlite for streamlit

--- orm/functions.py
+++ orm/functions.py
@@ -13,25 +13,22 @@ logger = logging.getLogger(__name__)
 def verify_user_credentials(username: str, password: str) -> bool:
     try:
         # Create a new database session
-        session = SessionLocal()
+        with SessionLocal() as session:
+            # Hash the password using SHA-256
+            hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
-        # Hash the password using SHA-256
-        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+            # Query to check if the username and hashed password exist in the users table
+            user = (
+                session.query(User)
+                .filter(func.lower(User.username) == username.lower(), User.password == hashed_password)
+                .one_or_none()
+            )
 
-        # Query to check if the username and hashed password exist in the users table
-        user = (
-            session.query(User)
-            .filter(func.lower(User.username) == username.lower(), User.password == hashed_password)
-            .first()
-        )
+            if user:
+                st.session_state.cookies["user_id"] = json.dumps(user.id)
 
-        st.session_state.cookies["user_id"] = json.dumps(user.id)
-
-        # Close the database session
-        session.close()
-
-        # Return True if the user exists, otherwise return False
-        return user is not None
+            # Return True if the user exists, otherwise return False
+            return user is not None
     except Exception as e:
         st.error(f"Error verifying user credentials: {e}")
         logger.error(f"Error verifying user credentials: {e}")
@@ -41,30 +38,20 @@ def verify_user_credentials(username: str, password: str) -> bool:
 def change_password(user_id: int, current_password: str, new_password: str) -> bool:
     try:
         # Create a new database session
-        session = SessionLocal()
-
-        # Retrieve the existing user from the database
-        user = session.query(User).filter(User.id == user_id).first()
-
-        current_password = hashlib.sha256(current_password.encode()).hexdigest()
-
-        # Verify the current password
-        if user and current_password == user.password:
+        with SessionLocal() as session:
             # Retrieve the existing user from the database
             user = session.query(User).filter(User.id == user_id).first()
 
-            # Update the user's password in the database
-            user.password = hashlib.sha256(new_password.encode()).hexdigest()
-            session.commit()
+            current_password_hashed = hashlib.sha256(current_password.encode()).hexdigest()
 
-            # Close the session
-            session.close()
-
-            return True
-        else:
-            session.close()
-
-            return False
+            # Verify the current password
+            if user and current_password_hashed == user.password:
+                # Update the user's password in the database
+                user.password = hashlib.sha256(new_password.encode()).hexdigest()
+                session.commit()
+                return True
+            else:
+                return False
     except Exception as e:
         st.error(f"Error changing password: {e}")
         logger.error(f"Error changing password: {e}")
@@ -141,15 +128,10 @@ def save_user_settings():
 def get_user(user_id):
     try:
         # Create a new database session
-        session = SessionLocal()
-
-        # Query to get the user by ID
-        user = session.query(User).filter(User.id == user_id).first()
-
-        # Close the session
-        session.close()
-
-        return user
+        with SessionLocal() as session:
+            # Query to get the user by ID
+            user = session.query(User).filter(User.id == user_id).first()
+            return user
     except Exception as e:
         st.error(f"Error getting user: {e}")
         logger.error(f"Error getting user: {e}")
@@ -162,21 +144,15 @@ def get_recent_messages():
         user_id = st.session_state.cookies.get("user_id")
 
         # Create a new database session
-        session = SessionLocal()
-
-        # Query to get the last 20 messages for the user, excluding those with an index greater than max_index
-        messages = (
-            session.query(Message)
-            .filter(Message.user_id == user_id, Message.id > max_index)
-            .order_by(Message.created_at.desc())
-            .limit(20)
-            .all()
-        )
-
-        # Close the session
-        session.close()
-
-        messages.reverse()
+        with SessionLocal() as session:
+            # Query to get the last 20 messages for the user, excluding those with an index greater than max_index
+            messages = (
+                session.query(Message)
+                .filter(Message.user_id == user_id, Message.id > max_index)
+                .order_by(Message.created_at.asc())
+                .limit(20)
+                .all()
+            )
 
         return messages
     except Exception as e:
@@ -189,13 +165,9 @@ def delete_all_messages():
         user_id = st.session_state.cookies.get("user_id")
 
         # Create a new database session
-        session = SessionLocal()
-
-        session.query(Message).filter(Message.user_id == user_id).delete()
-        session.commit()
-
-        # Close the session
-        session.close()
+        with SessionLocal() as session:
+            session.query(Message).filter(Message.user_id == user_id).delete()
+            session.commit()
 
         st.toast("All messages deleted successfully!")
 
