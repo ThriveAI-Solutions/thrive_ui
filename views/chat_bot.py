a4884723f857d9885a4c666deb7a6469ed342e22
Kyle Root <kyle@wellconnected.co>
2025-06-20 20:31:03 -0400

    finishing up the magic functions that are built out for now.

--- views/chat_bot.py
+++ views/chat_bot.py
@@ -4,7 +4,16 @@ import time
 import pandas as pd
 import streamlit as st
 from ethical_guardrails_lib import get_ethical_guideline
-import utils.chat_bot_helper as cbh
+from utils.chat_bot_helper import (
+    set_question,
+    vn,
+    get_unique_messages,
+    render_message,
+    add_message,
+    call_llm,
+    get_chart,
+    get_followup_questions,
+)
 from orm.functions import get_recent_messages, save_user_settings, set_user_preferences_in_session_state
 from orm.models import Message
 from utils.communicate import listen, speak
@@ -89,7 +98,7 @@ with st.sidebar.expander("Settings"):
     st.checkbox("LLM Fallback on Error", value=st.session_state.get("llm_fallback", False), key="temp_llm_fallback")
     st.button("Save", on_click=save_settings_on_click, use_container_width=True)
 
-st.sidebar.button("Reset", on_click=lambda: cbh.set_question(None), use_container_width=True, type="primary")
+st.sidebar.button("Reset", on_click=lambda: set_question(None), use_container_width=True, type="primary")
 
 if st.session_state.get("voice_input", True):
     with st.sidebar.popover("🎤 Speak Your Question", use_container_width=True):
@@ -100,17 +109,17 @@ if st.session_state.get("voice_input", True):
             else:
                 st.error("No input detected.")
             if text:
-                cbh.set_question(text, False)
+                set_question(text, False)
 
 # Show suggested questions
 if st.session_state.get("show_suggested", True):
     with st.sidebar.popover("Click to show suggested questions", use_container_width=True):
-        questions = cbh.vn.generate_questions()
+        questions = vn.generate_questions()
         for i, question in enumerate(questions):
             time.sleep(0.05)
             button = st.button(
                 question,
-                on_click=cbh.set_question,
+                on_click=set_question,
                 args=(question, False),
                 key=f"suggested_question_{i}",
                 use_container_width=True,
@@ -120,12 +129,12 @@ if st.session_state.get("show_suggested", True):
 if st.session_state.get("show_question_history", True):
     with st.sidebar:
         st.title("Question History")
-    filtered_messages = cbh.get_unique_messages()
+    filtered_messages = get_unique_messages()
     if len(filtered_messages) > 0:
         for past_question in filtered_messages:
             st.sidebar.button(
                 past_question.content,
-                on_click=cbh.set_question,
+                on_click=set_question,
                 args=(past_question.content,),
                 use_container_width=True,
             )
@@ -144,7 +153,7 @@ if st.session_state.messages == []:
 # Populate messages in the chat message component everytime the streamlit is run
 index = 0
 for message in st.session_state.messages:
-    cbh.render_message(message, index)
+    render_message(message, index)
     index = index + 1
 
 # Always show chat input
@@ -152,7 +161,7 @@ chat_input = st.chat_input("Ask me a question about your data")
 
 ######### Handle new chat input #########
 if chat_input:
-    cbh.set_question(chat_input)
+    set_question(chat_input)
 
 # Get question from session state
 my_question = st.session_state.get("my_question", None)
@@ -177,8 +186,8 @@ if my_question:
             guardrail_score,
             guardrail_sentence,
         )
-        cbh.add_message(Message(RoleType.ASSISTANT, guardrail_sentence, MessageType.ERROR, "", my_question))
-        cbh.call_llm(my_question)
+        add_message(Message(RoleType.ASSISTANT, guardrail_sentence, MessageType.ERROR, "", my_question))
+        call_llm(my_question)
         st.stop()
     if guardrail_score >= 3:
         logger.warning(
@@ -187,7 +196,7 @@ if my_question:
             guardrail_score,
             guardrail_sentence,
         )
-        cbh.add_message(Message(RoleType.ASSISTANT, guardrail_sentence, MessageType.ERROR, "", my_question))
+        add_message(Message(RoleType.ASSISTANT, guardrail_sentence, MessageType.ERROR, "", my_question))
         st.stop()
 
     # write an acknowledgment message to
@@ -195,23 +204,23 @@ if my_question:
     with st.chat_message(RoleType.ASSISTANT.value):
         st.write(random_acknowledgment)
 
-    sql, elapsed_time = cbh.vn.generate_sql(question=my_question)
+    sql, elapsed_time = vn.generate_sql(question=my_question)
     st.session_state.my_question = None
 
     if sql:
-        if cbh.vn.is_sql_valid(sql=sql):
+        if vn.is_sql_valid(sql=sql):
             if st.session_state.get("show_sql", True):
-                cbh.add_message(Message(RoleType.ASSISTANT, sql, MessageType.SQL, sql, my_question, None, elapsed_time))
+                add_message(Message(RoleType.ASSISTANT, sql, MessageType.SQL, sql, my_question, None, elapsed_time))
         else:
             logger.debug("sql is not valid")
-            cbh.add_message(Message(RoleType.ASSISTANT, sql, MessageType.ERROR, sql, my_question, None, elapsed_time))
+            add_message(Message(RoleType.ASSISTANT, sql, MessageType.ERROR, sql, my_question, None, elapsed_time))
             # TODO: not sure if calling the LLM here is the correct spot or not, it seems to be necessary
             if st.session_state.get("llm_fallback", True):
                 logger.debug("fallback to LLM")
-                cbh.call_llm(my_question)
+                call_llm(my_question)
             st.stop()
 
-        df = cbh.vn.run_sql(sql=sql)
+        df = vn.run_sql(sql=sql)
 
         # if sql doesn't return a dataframe, stop
         if not isinstance(df, pd.DataFrame):
@@ -221,23 +230,23 @@ if my_question:
 
         if st.session_state.get("show_table", True):
             df = st.session_state.get("df")
-            cbh.add_message(Message(RoleType.ASSISTANT, df, MessageType.DATAFRAME, sql, my_question))
+            add_message(Message(RoleType.ASSISTANT, df, MessageType.DATAFRAME, sql, my_question))
 
         if st.session_state.get("show_chart", True):
-            cbh.get_chart(my_question, sql, df)
+            get_chart(my_question, sql, df)
 
         if st.session_state.get("show_summary", True) or st.session_state.get("speak_summary", True):
-            summary, elapsed_time = cbh.vn.generate_summary(question=my_question, df=df)
+            summary, elapsed_time = vn.generate_summary(question=my_question, df=df)
             if summary is not None:
                 if st.session_state.get("show_summary", True):
-                    cbh.add_message(
+                    add_message(
                         Message(RoleType.ASSISTANT, summary, MessageType.SUMMARY, sql, my_question, df, elapsed_time)
                     )
 
                 if st.session_state.get("speak_summary", True):
                     speak(summary)
             else:
-                cbh.add_message(
+                add_message(
                     Message(
                         RoleType.ASSISTANT,
                         "Could not generate a summary",
@@ -252,9 +261,9 @@ if my_question:
                     speak("Could not generate a summary")
 
         if st.session_state.get("show_followup", True):
-            cbh.get_followup_questions(my_question, sql, df)
+            get_followup_questions(my_question, sql, df)
     else:
-        cbh.add_message(
+        add_message(
             Message(
                 RoleType.ASSISTANT,
                 "I wasn't able to generate SQL for that question",
@@ -264,5 +273,5 @@ if my_question:
             )
         )
         if st.session_state.get("llm_fallback", True):
-            cbh.call_llm(my_question)
+            call_llm(my_question)
 ######### Handle new chat input #########
